426e747f022181f95f3334aa8d6153156673d2dd
ds19122 <117936984+ds19122@users.noreply.github.com>
2022-11-10 20:09:58 +0200

    Add files via upload

---
+++ Main.groovy
@@ -0,0 +1,8 @@
+static void main(String[] args) {
+  static void(Integer args)
+  char onoma_etairias , dieuthunsh ;
+  int upallhloi ;
+
+  System.out.println( onoma_etairias , dieuthunsh , upallhloi);
+
+}
